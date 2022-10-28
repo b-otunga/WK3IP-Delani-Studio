a421e8cbf56a93b46ea1aab44fffa5c36b3fd710
Bill <billotungah@gmail.com>
2022-10-28 19:19:40 +0300

    set porfolio section opacity upon hover

---
+++ javascript/javascript.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $("#design_pic").click(function(){
+      $("#design_pic").hide(1200);
+      $("#design-text").toggle(1200);
+      $("#design").show(1200);
+    })
+    $("#design").click(function(){
+      $("#design-text").hide(1200);
+      $("#design").toggle(1200);
+      $("#design").show(1200);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#development_pic").click(function(){
+      $("#development_pic").hide(1200);
+      $("#development").toggle(1200);
+      $("#development").show(1200);
+    })
+    $("#development").click(function(){
+      $("#development").hide(1200);
+      $("#development_pic").toggle(1200);
+      $("#development_pic").show(1200);
+    });
+  });
+
+
+  $(document).ready(function(){
+    $("#product_pic").click(function(){
+      $("#product_pic").hide(1200);
+      $("#product").toggle(1200);
+      $("#product").show(1200);
+    })
+    $("#product").click(function(){
+      $("#product").hide(1200);
+      $("#product_pic").toggle(1200);
+      $("#product_pic").show(1200);
+    });
+  });
