b59a7f84f5f738a902428bc85a7c3ac552e7f73e
Bill <billotungah@gmail.com>
2022-10-29 20:36:57 +0300

    add if statement in jquery code to make porfolio descriptions responsive

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -74,18 +74,26 @@ $(document).ready(function () {
 
     $(".porfolio-pic1").hover(
         function () {
+            $(this).css({cursor: "pointer"})
             $("#work-one").show()
-                .css({ color: "black", opacity: "1" })
+                .css({ color: "white", "font-size": "122%"})
+                if($(window).width() < 822) {
+                    $("#work-one").css({ color: "white", "font-size": "50%", width: "60%"})}
+
         },
         function () {
             $("#work-one").hide()
+    
         }
     )
 
     $(".porfolio-pic2").hover(
         function () {
+            $(this).css({cursor: "pointer"})
             $("#work-2").show()
-                .css({ color: "black", opacity: "1" })
+            .css({ color: "white", "font-size": "122%"})
+            if($(window).width() < 822) {
+                $("#work-2").css({ color: "white", "font-size": "50%", width: "60%"})}
         },
         function () {
             $("#work-2").hide()
@@ -94,8 +102,11 @@ $(document).ready(function () {
 
     $(".porfolio-pic3").hover(
         function () {
+            $(this).css({cursor: "pointer"})
             $("#work-3").show()
-                .css({ color: "black", opacity: "1" })
+            .css({ color: "white", "font-size": "122%"})
+            if($(window).width() < 822) {
+                $("#work-3").css({ color: "white", "font-size": "50%", width: "60%"})}
         },
         function () {
             $("#work-3").hide()
@@ -104,8 +115,11 @@ $(document).ready(function () {
 
     $(".porfolio-pic4").hover(
         function () {
+            $(this).css({cursor: "pointer"})
             $("#work-4").show()
-                .css({ color: "black", opacity: "1" })
+            .css({ color: "white", "font-size": "122%"})
+            if($(window).width() < 822) {
+                $("#work-4").css({ color: "white", "font-size": "50%", width: "60%"})}
         },
         function () {
             $("#work-4").hide()
@@ -114,8 +128,11 @@ $(document).ready(function () {
 
     $(".porfolio-pic5").hover(
         function () {
+            $(this).css({cursor: "pointer"})
             $("#work-5").show()
-                .css({ color: "black", opacity: "1" })
+            .css({ color: "white", "font-size": "122%"})
+            if($(window).width() < 822) {
+                $("#work-5").css({ color: "white", "font-size": "50%", width: "60%"})}
         },
         function () {
             $("#work-5").hide()
@@ -124,8 +141,11 @@ $(document).ready(function () {
 
     $(".porfolio-pic6").hover(
         function () {
+            $(this).css({cursor: "pointer"})
             $("#work-6").show()
-                .css({ color: "black", opacity: "1" })
+            .css({ color: "white", "font-size": "122%"})
+            if($(window).width() < 822) {
+                $("#work-6").css({ color: "white", "font-size": "50%", width: "60%"})}
         },
         function () {
             $("#work-6").hide()
@@ -134,8 +154,11 @@ $(document).ready(function () {
 
     $(".porfolio-pic7").hover(
         function () {
+            $(this).css({cursor: "pointer"})
             $("#work-7").show()
-                .css({ color: "black", opacity: "1" })
+            .css({ color: "white", "font-size": "122%"})
+            if($(window).width() < 822) {
+                $("#work-7").css({ color: "white", "font-size": "50%", width: "60%"})}
         },
         function () {
             $("#work-7").hide()
@@ -144,17 +167,23 @@ $(document).ready(function () {
 
     $(".porfolio-pic8").hover(
         function () {
+            $(this).css({cursor: "pointer"})
             $("#work-8").show()
-                .css({ color: "black", opacity: "1" })
+            .css({ color: "white", "font-size": "122%"})
+            if($(window).width() < 822) {
+                $("#work-8").css({ color: "white", "font-size": "50%", width: "60%"})}
         },
         function () {
             $("#work-8").hide()
         }
     )
 
+
     $("#contact-form").submit(
         function () {
             alert("Your message was received and we will respond in no time. Peace!");
         }
     )
+
+  
 })
