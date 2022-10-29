30c3630760d77c89f32bbce2c06c5011fb7ccba2
Bill <billotungah@gmail.com>
2022-10-29 21:19:49 +0300

    delete commented out code

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,42 +1,3 @@
-// $(document).ready(function(){
-//     $("#design_pic").click(function(){
-//       $("#design_pic").hide(1200);
-//       $("#design-text").toggle(1200);
-//       $("#design").show(1200);
-//     })
-//     $("#design").click(function(){
-//       $("#design-text").hide(1200);
-//       $("#design").toggle(1200);
-//       $("#design").show(1200);
-//     });
-//   });
-
-//   $(document).ready(function(){
-//     $("#development_pic").click(function(){
-//       $("#development_pic").hide(1200);
-//       $("#development").toggle(1200);
-//       $("#development").show(1200);
-//     })
-//     $("#development").click(function(){
-//       $("#development").hide(1200);
-//       $("#development_pic").toggle(1200);
-//       $("#development_pic").show(1200);
-//     });
-//   });
-
-
-//   $(document).ready(function(){
-//     $("#product_pic").click(function(){
-//       $("#product_pic").hide(1200);
-//       $("#product").toggle(1200);
-//       $("#product").show(1200);
-//     })
-//     $("#product").click(function(){
-//       $("#product").hide(1200);
-//       $("#product_pic").toggle(1200);
-//       $("#product_pic").show(1200);
-//     });
-
 $(document).ready(function () {
 
     $("#design-pic").click(function () {
@@ -78,7 +39,7 @@ $(document).ready(function () {
             $("#work-one").show()
                 .css({ color: "white", "font-size": "122%"})
                 if($(window).width() < 822) {
-                    $("#work-one").css({ color: "white", "font-size": "50%", width: "60%"})}
+                    $("#work-one").css({ color: "white", "font-size": "50%", width: "63%", "border-width": "2px"})}
 
         },
         function () {
@@ -93,7 +54,7 @@ $(document).ready(function () {
             $("#work-2").show()
             .css({ color: "white", "font-size": "122%"})
             if($(window).width() < 822) {
-                $("#work-2").css({ color: "white", "font-size": "50%", width: "60%"})}
+                $("#work-2").css({ color: "white", "font-size": "50%", width: "63%", "border-width": "2px"})}
         },
         function () {
             $("#work-2").hide()
@@ -106,7 +67,7 @@ $(document).ready(function () {
             $("#work-3").show()
             .css({ color: "white", "font-size": "122%"})
             if($(window).width() < 822) {
-                $("#work-3").css({ color: "white", "font-size": "50%", width: "60%"})}
+                $("#work-3").css({ color: "white", "font-size": "50%", width: "63%", "border-width": "2px"})}
         },
         function () {
             $("#work-3").hide()
@@ -119,7 +80,7 @@ $(document).ready(function () {
             $("#work-4").show()
             .css({ color: "white", "font-size": "122%"})
             if($(window).width() < 822) {
-                $("#work-4").css({ color: "white", "font-size": "50%", width: "60%"})}
+                $("#work-4").css({ color: "white", "font-size": "50%", width: "63%", "border-width": "2px"})}
         },
         function () {
             $("#work-4").hide()
@@ -132,7 +93,7 @@ $(document).ready(function () {
             $("#work-5").show()
             .css({ color: "white", "font-size": "122%"})
             if($(window).width() < 822) {
-                $("#work-5").css({ color: "white", "font-size": "50%", width: "60%"})}
+                $("#work-5").css({ color: "white", "font-size": "50%", width: "63%", "border-width": "2px"})}
         },
         function () {
             $("#work-5").hide()
@@ -145,7 +106,7 @@ $(document).ready(function () {
             $("#work-6").show()
             .css({ color: "white", "font-size": "122%"})
             if($(window).width() < 822) {
-                $("#work-6").css({ color: "white", "font-size": "50%", width: "60%"})}
+                $("#work-6").css({ color: "white", "font-size": "50%", width: "63%", "border-width": "2px"})}
         },
         function () {
             $("#work-6").hide()
@@ -158,7 +119,7 @@ $(document).ready(function () {
             $("#work-7").show()
             .css({ color: "white", "font-size": "122%"})
             if($(window).width() < 822) {
-                $("#work-7").css({ color: "white", "font-size": "50%", width: "60%"})}
+                $("#work-7").css({ color: "white", "font-size": "50%", width: "63%", "border-width": "2px"})}
         },
         function () {
             $("#work-7").hide()
@@ -171,7 +132,7 @@ $(document).ready(function () {
             $("#work-8").show()
             .css({ color: "white", "font-size": "122%"})
             if($(window).width() < 822) {
-                $("#work-8").css({ color: "white", "font-size": "50%", width: "60%"})}
+                $("#work-8").css({ color: "white", "font-size": "50%", width: "63%", "border-width": "2px"})}
         },
         function () {
             $("#work-8").hide()
